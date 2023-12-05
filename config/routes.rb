723ffd87d50a399a187ba69db86a057c9833268b
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-12-05 18:10:13 +0100

    Update root and update the view file to have id root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'greetings#api_random'
+  get '/greetings/api-random', to: 'greetings#api_random'
+  root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
